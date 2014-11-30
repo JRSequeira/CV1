95698b00400d6f129c3ed0787fa2639dd716a4ed
vasco-santos <vasco.santos@ua.pt>
2014-11-30 00:45:09 +0000

    AngleRotation

---
+++ OpenGL_ex_16/angteRotation.h
@@ -0,0 +1,24 @@
+/*
+ * angleRotation.h
+ *
+ * Ficheiro cabecalho do modulo de rotacao dos ponteiros
+ */
+
+
+#ifndef _angleRotation_h
+#define _angleRotation_h
+
+
+/* ----------------------------------------------------------------------- */
+
+float hour(void);
+
+float minute(void);
+
+float second(void);
+
+
+/* ----------------------------------------------------------------------- */
+
+
+#endif
